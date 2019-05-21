5c7fb0580bce2742751ea4fce55e5137e7ee3fd7
Letschers <eduardoletsch@hotmail.com>
2019-05-20 23:55:19 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log('Its working');
+
+var Twit = require('twit');
+var keys = require('./keys');
+
+var T = new Twit(keys);
+
+
+setInterval(tweet(), 1000*60*60*24);
+// 1000*60*60*24 = 1 day
+
+function tweet() {
+    var date = new Date();
+
+    var number = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    
+    var finalDate = new Date('07/15/2019');
+    var todayDate = new Date(month + 1 + '/' + number + '/' + year);
+    
+    const diffTime = Math.abs(finalDate.getTime() - todayDate.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    
+    var tweetText = {
+        status: "Faltam " + diffDays + " para o Nathan sair do emprego"
+    }
+
+    function tweeted(err, data, response) {
+        if (err) {
+            console.log('error');
+        } else {
+            console.log('It worked');
+        }
+    }
+
+    T.post('statuses/update', tweetText, tweeted);
+}
---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:           'Hr1kz3qMxCX7VR6ZQBCXMTmdZ',
+    consumer_secret:        'gWsW6eBabEyosZ2OcoE2LkCsYOaXM1uRB5wFSCwWpqF2ipSNcI', 
+    access_token:           '1130659797601325057-EE70yos4vUML6YyJBbXx1u4MPn98eT',
+    access_token_secret:    'IUbIp34yXSFAKkrnSNmQJIAd4GKYN0mKvUr371DH8qGT7'
+}
